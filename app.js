01a84f2a98d0068604e1d589cf4126c4fa7d964f
Derek Bamford <foxhound34@hotmail.com>
2023-07-05 21:00:02 -0400

    MVC Routing

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ var travelRouter = require('./app_server/routes/travel');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+
 // register handlebars partials (https://www.npmjs.com/package/hbs)
 hbs.registerPartials(path.join(__dirname, 'app_server','views/partials'));
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
