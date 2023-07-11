eb746f55edec58385f4e80ef408b77424aa407db
Derek Bamford <foxhound34@hotmail.com>
2023-07-11 18:36:39 -0400

    Static HTML to Templates with JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -4,7 +4,7 @@ const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel view */
 const travel = (reg, res) => {
-    res.render('travel', { title: pageTitle, trips});
+    res.render('travel', { title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
