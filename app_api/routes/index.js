9861bcfe7c454a044fbe77128322863af262447e
Derek Bamford <foxhound34@hotmail.com>
2023-08-06 16:29:45 -0400

    Edit Trip

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -6,12 +6,12 @@ const tripsController = require("../controllers/trips");
 router
   .route("/trips")
   .get(tripsController.tripsList)
-  .post(tripsController.tripsAddTrip);
+  .post(auth, tripsController.tripsAddTrip);
 
 router
   .route("/trips/:tripCode")
   .get(tripsController.tripsList)
-  .put(tripsController.tripsUpdateTrip);
+  .put(auth, tripsController.tripsUpdateTrip);
 
 router.route("/trips/:tripCode").get(tripsController.tripsFindCode);
 
